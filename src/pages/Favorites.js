54b1002a94bcec5b288ac4e55cfdf28623ed0d5b
Marcel Jamecny <jamecny.m@tutanota.com>
2023-03-04 11:37:42 +0100

    add copy when no favorites saved

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,64 +1,76 @@
-import { FaRegTrashAlt, FaRegClock, FaRegCalendarAlt } from 'react-icons/fa'
-import Button from '../components/Button'
+import { FaRegTrashAlt, FaRegClock, FaRegCalendarAlt } from "react-icons/fa"
+import Button from "../components/Button"
 
 const Favorites = (props) => {
   return (
     <section className="section-vods">
-      <Button dispatch={props.dispatch} />
-      <div className="vods">
-        {props.state.favs.map((fav) => {
-          const { id, thumbnail_url, url, title, published_at, duration } = fav
+      {props.state.favs.length !== 0 ? (
+        <div>
+          <Button dispatch={props.dispatch} />
+          <div className="vods">
+            {props.state.favs.map((fav) => {
+              const { id, thumbnail_url, url, title, published_at, duration } =
+                fav
 
-          const final_src = thumbnail_url.replace(
-            /%{width}x%{height}/g,
-            '1280x720'
-          )
+              const final_src = thumbnail_url.replace(
+                /%{width}x%{height}/g,
+                "1280x720"
+              )
 
-          const date = new Date(published_at)
+              const date = new Date(published_at)
 
-          const [month, day, year] = [
-            date.getMonth(),
-            date.getDate(),
-            date.getFullYear(),
-          ]
+              const [month, day, year] = [
+                date.getMonth(),
+                date.getDate(),
+                date.getFullYear(),
+              ]
 
-          return (
-            <div className="one-vod" key={id}>
-              <img className="image" src={final_src} alt="thumbnail" />
-              <div className="box">
-                <h2 className="title">
-                  <a className="link" href={url}>
-                    {title}
-                  </a>
-                </h2>
-                <div className="text-box">
-                  <p className="date">
-                    <FaRegCalendarAlt className="icon" />
-                    <span>{`${day}/${month + 1}/${year}`}</span>
-                  </p>
-                  <button
-                    onClick={() =>
-                      props.dispatch({
-                        type: 'REMOVE_FAV',
-                        payload: id,
-                      })
-                    }
-                    className="del-btn"
-                    data-id={id}
-                  >
-                    <FaRegTrashAlt className="icon-fav" />
-                  </button>
+              return (
+                <div className="one-vod" key={id}>
+                  <img className="image" src={final_src} alt="thumbnail" />
+                  <div className="box">
+                    <h2 className="title">
+                      <a className="link" href={url}>
+                        {title}
+                      </a>
+                    </h2>
+                    <div className="text-box">
+                      <p className="date">
+                        <FaRegCalendarAlt className="icon" />
+                        <span>{`${day}/${month + 1}/${year}`}</span>
+                      </p>
+                      <button
+                        onClick={() =>
+                          props.dispatch({
+                            type: "REMOVE_FAV",
+                            payload: id,
+                          })
+                        }
+                        className="del-btn"
+                        data-id={id}
+                      >
+                        <FaRegTrashAlt className="icon-fav" />
+                      </button>
 
-                  <p className="duration">
-                    <FaRegClock className="icon" />
-                    <span>{duration}</span>
-                  </p>
+                      <p className="duration">
+                        <FaRegClock className="icon" />
+                        <span>{duration}</span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+              )
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="novods">
+          Don't have any favorite videos saved yet? No problem! Just browse
+          selection of videos from any Twitch streamer and click the "checkmark"
+          button on any video you want to save for later.
+        </div>
+      )}
+
       <div className="spinner"></div>
     </section>
   )
