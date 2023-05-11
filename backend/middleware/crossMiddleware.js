d37a78c1cff458a14c0ebbec4a45800a173f58fd
mjamecny <jamecny.m@tutanota.com>
2023-05-11 21:26:32 +0200

    add headers for preflight request

--- backend/middleware/crossMiddleware.js
+++ backend/middleware/crossMiddleware.js
@@ -1,12 +1,14 @@
 const allowCrossDomain = (err, req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    process.env.NODE_ENV === 'development'
-      ? ['http://localhost:3000']
-      : ['https://vodhub.netlify.app']
-  )
+  res.header('Access-Control-Allow-Origin', 'https://vodhub.netlify.app')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
 
-  next()
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
 }
 
 module.exports = { allowCrossDomain }
