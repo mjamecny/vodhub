39ea080a3456616dd0d2f0cf27023fd0fd3116ee
mjamecny <jamecny.m@tutanota.com>
2023-05-11 21:25:23 +0200

    add headers for preflight request

--- backend/middleware/crossMiddleware.js
+++ backend/middleware/crossMiddleware.js
@@ -1,16 +1,3 @@
-const asyncHandler = require('express-async-handler')
-
-// const allowCrossDomain = asyncHandler(async (req, res, next) => {
-//   res.header(
-//     'Access-Control-Allow-Origin',
-//     process.env.NODE_ENV === 'development'
-//       ? 'http://localhost:3000'
-//       : 'https://vodhub.netlify.app'
-//   )
-
-//   next()
-// })
-
 const allowCrossDomain = (err, req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
