985efa56908b309d8d943cd043284bc6fd1c8016
mjamecny <jamecny.m@tutanota.com>
2023-05-24 11:18:47 +0200

    edit error controller

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -27,57 +27,55 @@ const handleJWTExpiredError = () => {
 
 const sendErrorDev = (err, req, res) => {
   // A) API
-  if (req.originalUrl.startsWith('/api')) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    })
-  }
-  // B) RENDERED WEBSITE
-  console.error('ERROR', err)
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message,
+  return res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
   })
+
+  // B) RENDERED WEBSITE
+  // console.error('ERROR', err)
+  // return res.status(err.statusCode).render('error', {
+  //   title: 'Something went wrong!',
+  //   msg: err.message,
+  // })
 }
 
 const sendErrorProd = (err, req, res) => {
   // A) API
-  if (req.originalUrl.startsWith('/api')) {
-    // a) Operational, trusted error: send message to client
-    if (err.isOperational) {
-      return res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      })
-    }
-    // b) Programming or other unknown error: don't leak error details
-    // 1) Log error
-    console.error('ERROR', err)
-    // 2) Send generic message
-    return res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!',
-    })
-  }
-  // B) RENDERED WEBSITE
+  // a) Operational, trusted error: send message to client
   if (err.isOperational) {
-    // a) Operational, trusted error: send message to client
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
-      msg: err.message,
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
     })
   }
   // b) Programming or other unknown error: don't leak error details
   // 1) Log error
   console.error('ERROR', err)
   // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: 'Please try again later.',
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went very wrong!',
   })
+
+  // B) RENDERED WEBSITE
+  // if (err.isOperational) {
+  //   // a) Operational, trusted error: send message to client
+  //   return res.status(err.statusCode).render('error', {
+  //     title: 'Something went wrong!',
+  //     msg: err.message,
+  //   })
+  // }
+  // b) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  // console.error('ERROR', err)
+  // 2) Send generic message
+  // return res.status(err.statusCode).render('error', {
+  //   title: 'Something went wrong!',
+  //   msg: 'Please try again later.',
+  // })
 }
 
 module.exports = (err, req, res, next) => {
